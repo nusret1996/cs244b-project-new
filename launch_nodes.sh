d6433929679edc40687900f7dbba46f3e47b7d14
sher222 <sherylh@stanford.edu>
2024-05-16 02:47:38 -0700

    add script to launch multiple nodes

---
+++ launch_nodes.sh
@@ -0,0 +1,4 @@
+LAUNCH_TIME=$(date -v+2S -u +%H:%M:%S)
+echo "LAUNCH_TIME $LAUNCH_TIME"
+osascript -e 'tell app "Terminal" to do script "cd /Users/sherylhsu/school/cs244b/cs244b-project/build_dir; ./main '"${LAUNCH_TIME}"' 200 ../peerconfig 0"'
+osascript -e 'tell app "Terminal" to do script "cd /Users/sherylhsu/school/cs244b/cs244b-project/build_dir; ./main '"${LAUNCH_TIME}"' 200 ../peerconfig 1"'
